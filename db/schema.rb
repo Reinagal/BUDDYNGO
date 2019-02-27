655aa3c4d548f5a6432d8fbc60485848b934df0f
GuillaumeLaganier <g.laganier@gmail.com>
2019-02-27 10:33:25 +0100

    step column added to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_163209) do
+ActiveRecord::Schema.define(version: 2019_02_27_092016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_163209) do
     t.string "theme"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "step", default: 1
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
