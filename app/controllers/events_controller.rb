b6fbde041cc9f6b0a8091051e96db18f0b2c51e8
GuillaumeLaganier <g.laganier@gmail.com>
2019-02-27 11:10:06 +0100

    events index to be continued after seed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
   def index
-    @myevents = Event.all.select { |obj| obj.user_id == current_user.id }
+    @myevents = Event.where(user_id: current_user.id)
   end
 end
