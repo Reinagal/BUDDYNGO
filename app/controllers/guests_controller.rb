547b696d26325b46d43e6ce755b12708f0424cdb
Olivier Colombani <olcolombani@gmail.com>
2019-02-28 18:47:44 +0100

    form add guest form, modif controller guest & guest model

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,8 @@
 class GuestsController < ApplicationController
   def new
     @guest = Guest.new
+    # @event = Event.find(params[:event_id])
+    # @guest = @event.guests.build
   end
 
   def create
@@ -8,6 +10,9 @@ class GuestsController < ApplicationController
     @guest = @event.guests.build(guest_params)
     @guest.event = @event
     @guest.save
+    # @event = Event.find(params[:event_id])
+    # @guest = @event.guests.create(guest_params)
+    # redirect_to edit_event_path(@event)
   end
 
   private
