9e2f6b4ef47f7c1773b36e1d078374af5490080a
Florent Horta <florenthorta@gmail.com>
2019-02-28 14:55:30 +0100

    the modals are connected

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :events
+  resources :choices
+
+  resources :polls
   resources :answers
 end
