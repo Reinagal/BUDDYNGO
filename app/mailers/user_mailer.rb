eddd317743fa4ba088ab18f122a1e8cc31576f18
GuillaumeLaganier <g.laganier@gmail.com>
2019-03-01 13:26:33 +0100

    mailer error fixed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   def votepush(user)
     @guest = user # Instance variable => available in view
 
-    mail(to: @guest.email, subject: "Vote for #{@event.name}")
+    mail(to: @guest.email, subject: "Test")
     # This will render a view in `app/views/user_mailer`!
   end
 end
