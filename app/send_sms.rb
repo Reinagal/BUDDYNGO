50464c8de599254da66bad426ec6b2bb84a80a2a
GuillaumeLaganier <g.laganier@gmail.com>
2019-03-04 11:36:50 +0100

    twilio test config, and postmark token security

---
+++ app/send_sms.rb
@@ -0,0 +1,14 @@
+require 'twilio-ruby'
+
+account_sid = { twil_sid: ENV['TWILIO_SID'] }
+auth_token = { twil_token: ENV['TWILIO_TOKEN'] }
+client = Twilio::REST::Client.new(account_sid, auth_token)
+
+from = { phone_number: ENV['TWILIO_NUMBER'] } # Your Twilio number
+to = '+33689571444' # Your mobile phone number
+
+client.messages.create(
+from: from,
+to: to,
+body: "Hey friend!"
+)
