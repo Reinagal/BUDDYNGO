ff51ccd26e5dadcf030949f26ab76f8da2eb1a7e
GuillaumeLaganier <g.laganier@gmail.com>
2019-03-04 13:38:13 +0100

    sms in local send to the right phone number functionnal

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,7 +1,7 @@
 class Guest < ApplicationRecord
   belongs_to :event
   has_many :answers
-  after_create :email_vote_link
+  after_create :email_vote_link, :sms_vote_link
   # validates :name, presence: true, length: { minimum: 5 }
   # validates :email, presence: true
 
@@ -12,6 +12,6 @@ class Guest < ApplicationRecord
   end
 
   def sms_vote_link
-    SendSmsService.call(self)
+    SendSmsService.new(self).call
   end
 end
