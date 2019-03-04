a9426de2affa9d3d2edaab7a992ab2d9c253e13f
GuillaumeLaganier <g.laganier@gmail.com>
2019-03-04 14:19:06 +0100

    sms in local send to the right phone number functionnal AND unactive

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,7 +1,7 @@
 class Guest < ApplicationRecord
   belongs_to :event
   has_many :answers
-  after_create :email_vote_link, :sms_vote_link
+  after_create :email_vote_link, #:sms_vote_link
   # validates :name, presence: true, length: { minimum: 5 }
   # validates :email, presence: true
 
