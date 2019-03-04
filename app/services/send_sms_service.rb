5ef71d4ba86e613197510e427277b826bcd57d4d
GuillaumeLaganier <g.laganier@gmail.com>
2019-03-04 16:29:11 +0100

    dont work on sms

--- app/services/send_sms_service.rb
+++ app/services/send_sms_service.rb
@@ -17,33 +17,23 @@ class SendSmsService
     to = @guest.phone_number # Guest mobile phone number
 
     host = ENV["HOST"] || "localhost:3000"
-    url = Rails.application.routes.url_helpers.new_event_guest_answer_url(@guest.event, @guest, host: host)
+    url1 = Rails.application.routes.url_helpers.new_event_guest_answer_url(@guest.event, @guest, host: host)
+    url2 = Rails.application.routes.url_helpers.edit_event_guest_answer_url(@guest.event, @guest, host: host)
 
-    client.messages.create(
-      from: from,
-      to: to,
-      body: "Hey #{@guest.name}, #{@guest.event.user.name} need you to click on this link to vote for #{@guest.event.name} #{url}"
-    )
-  end
+    # if @guest.event.step == 1
+      client.messages.create(
+        from: from,
+        to: to,
+        body: "Hey #{@guest.name}, #{@guest.event.user.name} need you to click on this link to vote for #{@guest.event.name} #{url1}"
+      )
+    # elsif @guest.event.step == 2
+    #   client.messages.create(
+    #     from: from,
+    #     to: to,
+    #     body: "Hey #{@guest.name}, #{@guest.event.user.name} need you to click on this link to vote one last time for #{@guest.event.name} #{url2}"
+    #   )
+    # else
 
-  def call2
-    account_sid = ENV['TWILIO_SID']
-
-    auth_token = ENV['TWILIO_TOKEN']
-
-    client = Twilio::REST::Client.new(account_sid, auth_token)
-
-    from = ENV['TWILIO_NUMBER']
-
-    to = '+33689571444' # Guest mobile phone number
-
-    host = ENV["HOST"] || "localhost:3000"
-    url = Rails.application.routes.url_helpers.edit_event_guest_answer_url(@guest.event, @guest, host: host)
-
-    client.messages.create(
-      from: from,
-      to: to,
-      body: "Hey #{@guest.name}, #{@guest.event.user.name} need you to click on this link to vote one last time for #{@guest.event.name} #{url}"
-    )
+    # end
   end
 end
