7b5a1d4c970dcd33da991140bcca25b31901b53f
Nicolas Essayan <nicolas_essayan@yahoo.fr>
2019-03-05 15:33:29 +0100

    Sur la bonne route

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -40,8 +40,8 @@ class ChoicesController < ApplicationController
     end
   end
 
-  def createdestinationschoices
-
+  def newdestinationchoices
+    @poll = Poll.find(params[:poll_id])
   end
 
   def destroy
