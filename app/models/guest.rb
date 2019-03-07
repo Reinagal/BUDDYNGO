3da280d88329d98943ad74ba3d32bf9a8b7dfd81
Florent Horta <florenthorta@gmail.com>
2019-03-07 12:45:52 +0100

    Phone number format ok

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,6 +1,41 @@
 class Guest < ApplicationRecord
   belongs_to :event
   has_many :answers
-  # validates :name, presence: true, length: { minimum: 5 }
-  # validates :email, presence: true
+  validates :name, presence: true
+  validate :mail_or_phone_number
+  validate :valid_french_phone_number
+  after_create :format_phone_number
+
+  def mail_or_phone_number
+    if self.phone_number == "" && self.email == ""
+      errors.add("You have to add an email or a phone number")
+    end
+  end
+
+  def valid_french_phone_number
+    if self.phone_number
+      if !self.phone_number.match(/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/)
+        errors.add("Please enter a valid french phone number")
+      end
+    else
+      true
+    end
+  end
+
+  def format_phone_number
+    if self.phone_number
+      stripped_phone_number = self.phone_number.gsub(' ','')
+      case
+        when stripped_phone_number.start_with?('+33')
+          then self.update phone_number: stripped_phone_number
+        when stripped_phone_number.start_with?('0033')
+          then self.update phone_number: stripped_phone_number[2..-1].insert(0, '+')
+        when stripped_phone_number.start_with?('06')
+          then self.update phone_number: stripped_phone_number[1..-1].insert(0, '+33')
+        when stripped_phone_number.start_with?('07')
+          then self.update phone_number: stripped_phone_number[1..-1].insert(0, '+33')
+      end
+    end
+  end
+
 end
